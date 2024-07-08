60be3535e5a340ac3c8c400719eed199a98ea687
AntonyDruon <druonantony@gmail.com>
2024-07-08 11:17:11 +0200

    add env files + connexion db

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 const App = () => {
